c53eaec1abee5b226014c23e02468b97cb5352df
Sergio <32018041+thAngryUnicorn@users.noreply.github.com>
2020-05-08 20:33:15 +0200

    Add files via upload

--- core.c
+++ core.c
@@ -62,7 +62,7 @@ void round_of_game(int matriz_adyacencia[N1+N2][N1+N2], Persona population_tot[N
 //estudio payoffs y nueva poblacion
 void study_results(int matriz_adyacencia[N1+N2][N1+N2], Persona population_tot[N1+N2]);//estudia resultados de la array y realiza los cambios a nuevo array
     double maximo_payoff(Persona population_tot[N1+N2]);//halla maximo payoff de entre todas las parejas
-    int compare_with_neighbour(Persona population_tot[N1+N2], Persona new_population[N1+N2],int matriz_adyacencia[N1+N2][N1+N2], int person, double max_diff_payoff)//compara payoff 2 vecinos
+    int compare_with_neighbour(Persona population_tot[N1+N2], Persona new_population[N1+N2],int matriz_adyacencia[N1+N2][N1+N2], int person, double max_diff_payoff);//compara payoff 2 vecinos
     void copy_chain(Persona *x, Persona *y, int length);//copia un array de structs en otro
 
 
@@ -71,7 +71,7 @@ void study_results(int matriz_adyacencia[N1+N2][N1+N2], Persona population_tot[N
 
 
 
-
+double porcentaje_colaboradores(Persona population_tot[N1+N2]);
 
 
 
@@ -81,7 +81,8 @@ int matriz_adyacencia[N1+N2][N1+N2]; //inicializar matriz adyacencia
 
 
 int main(){
-    srand(time(NULL))
+    srand(time(NULL));
+    printf("Ha gaaaayyyyyyyyyyy");
 }
 
 
@@ -141,13 +142,16 @@ void matrix_to_0(int matriz_adyacencia[N1+N2][N1+N2]){  //Inicializa la matriz a
     }
 }
 
+
 void copy_chain(Persona *x, Persona *y, int length){    //copia contenidos una struct en otra
     for(int i=0;i<length;i++){
-        y[i].group=x[i].group;
         y[i].cooperate=x[i].cooperate;
         y[i].payoff=x[i].payoff;
     }
 }
+
+
+
 
 
 /*Añadir un readme con la matriz para que se vea de forma visual*/
@@ -353,13 +357,13 @@ void study_results(int matriz_adyacencia[N1+N2][N1+N2], Persona population_tot[N
     Persona new_population[N1+N2];
     copy_chain(population_tot,new_population,N1+N2);//copiamos la buena en la temporar
 
-    max_diff_payoff = maximo_payoff(population_tot);//halla la maxima diferencia de payoff
+    double max_diff_payoff = maximo_payoff(population_tot);//halla la maxima diferencia de payoff
 
     for (int person = 0; person < N1 + N2 ; person++){
         compare_with_neighbour(population_tot, new_population, matriz_adyacencia, person, max_diff_payoff);
     }
     //ahora tendremos ya la nueva poblacion (cambios de cooperacion)
-    copy_chain(population_new, population_tot, N1+N2);//copiamos la temporal en la buena para tener la nueva
+    copy_chain(new_population, population_tot, N1+N2);//copiamos la temporal en la buena para tener la nueva
 
     //al acabar reset all payoff 0
     for(int i = 0; i < N1+N2; i++){
@@ -371,9 +375,9 @@ double maximo_payoff(Persona population_tot[N1+N2]){
     double max = 0;
     double min = 0;
     for(int person = 0; person < N1+N2; person++){
-        if (population_tot[persona].payoff > max)
+        if (population_tot[person].payoff > max)
             max = population_tot[person].payoff;
-        if (population_tot[persona].payoff < min)
+        if (population_tot[person].payoff < min)
             min = population_tot[person].payoff;
         
     }
@@ -398,20 +402,21 @@ int compare_with_neighbour(Persona population_tot[N1+N2], Persona new_population
 
     //seleccionamos al azar el vecino con el que comparar
     index_neigh += 1;
-    index_neigh = rand() % index_neigh //genera un integre aleatorio entre 0 e index_neig-1
+    index_neigh = rand() % index_neigh; //genera un integre aleatorio entre 0 e index_neig-1
 
     //Nuestro vecino sera: neighbours[index_neigh]
     int vecino = neighbours[index_neigh];
     double payoff_rival = population_tot[vecino].payoff;
-    double diff_payoff = payoff_rival-population_tot[person]  ;
+    double diff_payoff = payoff_rival-population_tot[person].payoff ;
     //si rival tiene mas dineros
-    if (diff_payoff > 0);
+    if (diff_payoff > 0){
         //proporcional a la diferneicai de dinero, metodo altura
-        double aleatoria = rand()/(double)(RAND_MAX+1);
+        double aleatoria = rand()/((double)RAND_MAX+1);
         double comparar = diff_payoff/max_diff_payoff;
         if (aleatoria < comparar){
-            new_population[person].cooperate = population_tot.tot[vecino].cooperate;
+            new_population[person].cooperate = population_tot[vecino].cooperate;
         }
+    }
     
 
 
@@ -424,10 +429,15 @@ int compare_with_neighbour(Persona population_tot[N1+N2], Persona new_population
 
 
 
-void copy_chain(Persona *x, Persona *y, int length){    //copia contenidos una struct en otra
-    for(int i=0;i<length;i++){
-        y[i].cooperate=x[i].cooperate;
-        y[i].payoff=x[i].payoff;
-    }
-}
 
+
+
+
+double porcentaje_colaboradores(Persona population_tot[N1+N2]){
+    double colaboradores = 0;
+    for (int i = 0; i < N1+N2; i++){
+        if (population_tot[i].cooperate)
+            colaboradores+=1;
+    }
+    return colaboradores/(N1+N2);
+}
